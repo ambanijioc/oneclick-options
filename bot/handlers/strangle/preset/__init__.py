6a7287a51286c3bb8ae748479e240b65128df99e
ambanijioc <ambanijiocine@gmail.com>
2025-10-22 19:41:32 +0530

    Create __init__.py

---
+++ bot/handlers/strangle/preset/__init__.py
@@ -0,0 +1,13 @@
+"""STRANGLE Trade Preset CRUD Operations"""
+
+from .create import *
+from .edit import *
+from .view import *
+from .delete import *
+
+__all__ = [
+    'strangle_preset_create',
+    'strangle_preset_edit',
+    'strangle_preset_view',
+    'strangle_preset_delete',
+]
