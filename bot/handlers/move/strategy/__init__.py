5bc22b8436091cda6e3eeb97757b0c75d30a74f7
ambanijioc <ambanijiocine@gmail.com>
2025-10-22 19:28:28 +0530

    Update __init__.py

--- bot/handlers/move/strategy/__init__.py
+++ bot/handlers/move/strategy/__init__.py
@@ -4,3 +4,11 @@ from .create import *
 from .edit import *
 from .view import *
 from .delete import *
+
+__all__ = [
+    'move_strategy_start',
+    'move_create_strategy',
+    'move_edit_strategy',
+    'move_view_strategies',
+    'move_delete_strategy',
+]
