2a256ac9cfaaf23823938ceda945cdf7b814cc38
ambanijioc <ambanijiocine@gmail.com>
2025-10-22 20:31:03 +0530

    Create __init__.py

---
+++ bot/handlers/straddle/preset/__init__.py
@@ -0,0 +1,13 @@
+"""STRADDLE Trade Preset CRUD Operations"""
+
+from .create import *
+from .edit import *
+from .view import *
+from .delete import *
+
+__all__ = [
+    'straddle_preset_create',
+    'straddle_preset_edit',
+    'straddle_preset_view',
+    'straddle_preset_delete',
+]
