840df5ed7f27ffded5fa0f971119391ea9f35cc8
ambanijioc <ambanijiocine@gmail.com>
2025-10-22 19:28:47 +0530

    Create __init__.py

---
+++ bot/handlers/move/preset/__init__.py
@@ -0,0 +1,13 @@
+"""MOVE Trade Preset CRUD Operations"""
+
+from .create import *
+from .edit import *
+from .view import *
+from .delete import *
+
+__all__ = [
+    'move_preset_create',
+    'move_preset_edit',
+    'move_preset_view',
+    'move_preset_delete',
+]
